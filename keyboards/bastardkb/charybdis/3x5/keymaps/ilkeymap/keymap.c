76daeac3b83029ddc31880e984d561264899c273
Immanuel Litzroth <immanuel.litzroth@gmail.com>
2025-08-16 09:32:08 +0200

    move ctl and esc.

--- keyboards/bastardkb/charybdis/3x5/keymaps/ilkeymap/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/ilkeymap/keymap.c
@@ -50,7 +50,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 //#define TAB_FUN LT(LAYER_FUNCTION, KC_TAB)
 //#define ENT_SYM LT(LAYER_SYMBOLS, KC_ENT)
 #define BSP_NUM LT(LAYER_NUMERAL, KC_BSPC)
-#define TAB_PTR LT(LAYER_POINTER, KC_TAB)
+#define TAB_PTR LT(LAYER_POINTER, KC_NO)
 
 #ifndef POINTING_DEVICE_ENABLE
 #    define DRGSCRL KC_NO
@@ -65,7 +65,7 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-         TAB_PTR, BSP_NUM, LCTL_T(KC_NO), /**/ LGUI_T(KC_ENT), LALT_T(KC_SPC)
+         LCTL_T(KC_TAB), BSP_NUM, TAB_PTR, /**/ LGUI_T(KC_ENT), LALT_T(KC_SPC)
 
 #define LAYOUT_NUMBER_BASE \
   KC_1, KC_2, KC_3, KC_4, KC_5, /**/ KC_6, KC_7, KC_8, KC_9, KC_0,      \
@@ -77,9 +77,9 @@ static uint16_t auto_pointer_layer_timer = 0;
 //QK_AUTO_SHIFT_UP	AS_UP	Raise the Auto Shift timeout variable (up)
 //QK_AUTO_SHIFT_REPORT	AS_RPT	Report your current Auto Shift timeout value
 #define LAYOUT_MOUSE_BASE \
-  KC_ESC, DRGSCRL, QK_MOUSE_WHEEL_UP, SNIPING, AS_DOWN, AS_UP, KC_PGUP, KC_UP, KC_PGDN, MO(LAYER_CONFIG), \
+  KC_NO, DRGSCRL, QK_MOUSE_WHEEL_UP, SNIPING, AS_DOWN, AS_UP, KC_PGUP, KC_UP, KC_PGDN, MO(LAYER_CONFIG), \
     AS_RPT, QK_MOUSE_WHEEL_LEFT, QK_MOUSE_WHEEL_DOWN,QK_MOUSE_WHEEL_RIGHT,   KC_NO,/**/ KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_NO, \
-   QK_LAYER_LOCK, KC_BTN3, KC_BTN2, KC_BTN1, KC_NO, /**/ KC_NO, KC_BTN1, KC_BTN2, KC_BTN3, KC_NO, \
+   QK_LAYER_LOCK, KC_BTN3, KC_BTN2, KC_BTN1, KC_NO, /**/ KC_NO, KC_BTN1, KC_BTN2, KC_BTN3, KC_ESC, \
     KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS
 
 #define LAYOUT_CONFIG_BASE \
