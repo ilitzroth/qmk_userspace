f4e017ac1652ce830345c2ab8876163c37c68d04
Immanuel Litzroth <immanuel.litzroth@gmail.com>
2025-11-05 12:03:56 +0100

    fixes.

--- keyboards/bastardkb/charybdis/3x5/keymaps/ilkeymap/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/ilkeymap/keymap.c
@@ -49,8 +49,8 @@ static uint16_t auto_pointer_layer_timer = 0;
 //#define SPC_NAV LT(LAYER_NAVIGATION, KC_SPC)
 //#define TAB_FUN LT(LAYER_FUNCTION, KC_TAB)
 //#define ENT_SYM LT(LAYER_SYMBOLS, KC_ENT)
-#define BSP_NUM LT(LAYER_NUMERAL, KC_BSPC)
-#define TAB_PTR LT(LAYER_POINTER, KC_EXECUTE)
+#define BSP_NUM LT(LAYER_NUMERAL, KC_EXECUTE)
+#define TAB_PTR LT(LAYER_POINTER, KC_NO)
 
 #ifndef POINTING_DEVICE_ENABLE
 #    define DRGSCRL KC_NO
@@ -65,12 +65,12 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
-         KC_LCTL, BSP_NUM, TAB_PTR, /**/ LGUI_T(KC_ENT), LALT_T(KC_SPC)
+         KC_LCTL, BSP_NUM, TAB_PTR, /**/ LGUI_T(KC_MPLY), LALT_T(KC_SPC)
 
 #define LAYOUT_NUMBER_BASE                                              \
   KC_1, KC_2, KC_3, KC_4, KC_5, /**/ KC_6, KC_7, KC_8, KC_9, KC_0,      \
     KC_GRAVE, KC_ESC, LSFT(KC_TAB), LCTL(KC_TAB), KC_LBRC, /**/ KC_RBRC, KC_MINS, KC_BSLS, KC_EQUAL, KC_QUOT, \
-    KC_F1, KC_F2, KC_BSPC, KC_TAB, KC_F12, /**/  KC_MPLY, KC_ENT, KC_SPC, KC_MPRV, KC_MNXT,  \
+    KC_NO, KC_NO, KC_BSPC, KC_TAB, KC_NO, /**/  KC_NO, KC_ENT, KC_SPC, KC_MPRV, KC_MNXT,  \
     KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS
 
 //QK_AUTO_SHIFT_DOWN	AS_DOWN	Lower the Auto Shift timeout variable (down)
@@ -83,7 +83,7 @@ static uint16_t auto_pointer_layer_timer = 0;
     KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS
 
 #define LAYOUT_CONFIG_BASE \
-  QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+  QK_BOOT, KC_F1, KC_F2, KC_F12, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
